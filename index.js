c1ad5c51dc3f07a6a0e5cd3c95fa02d8269ec7d0
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-02 23:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
 let h1 = document.createElement('h1');
-h1.setAttribute('id', 'victory')
+h1.id = "victory";
 document.body.appendChild(h1);
 main.remove();
 let newHeader = document.getElementById("h1#victory");
