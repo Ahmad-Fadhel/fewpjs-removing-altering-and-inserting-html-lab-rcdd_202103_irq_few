b261095f9a8ac3aaa5bc8092de8fb3cc5d23c65e
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-03 00:02:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 // Write your code here!
-let h1 = document.createElement('h1');
-h1.id = "victory";
-document.body.appendChild(h1);
 main.remove();
-let newHeader = document.getElementById("h1#victory");
 newHeader.innerHTML = "YOUR-NAME is the champion";
